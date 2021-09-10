448c139368c1bfafefd2d2f77beb34e8d211bb9c
Egor Yakubov <52618928+gorskii@users.noreply.github.com>
2021-09-10 20:48:38 +0300

    Much simplier 2.0-1D solution

--- training-2.0/homework-1/D/solution.py
+++ training-2.0/homework-1/D/solution.py
@@ -1,28 +1,9 @@
 def get_school_coordinates(coordinates):
-    current_sum = 0
-    for num in coordinates:
-        current_sum += abs(coordinates[0] - num)
-
-    minimal_sum = current_sum
-    school_coordinates = coordinates[0]
-    left, right = 0, len(coordinates)
-    while left <= right:
-        mid = (right + left) // 2
-        current_sum = 0
-        for num in coordinates:
-            current_sum += abs(coordinates[mid] - num)
-        if current_sum < minimal_sum:
-            minimal_sum = current_sum
-            school_coordinates = coordinates[mid]
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return school_coordinates
+    return coordinates[len(coordinates) // 2]
 
 
 if __name__ == '__main__':
     N = int(input())
-    coordinates = [int(x) for x in input().split()]
+    coordinates = list(map(int, input().split()))
 
     print(get_school_coordinates(coordinates))
